7957cbca884726121ee5d764a73178d434f00609
Stephen Mercer <u3158446@uni.canberra.edu.au>
2017-07-15 21:27:10 +1000

    Added Image picker, still errors in imagePickerController function (incomplete)

--- pontiyak/IntroViewController.swift
+++ pontiyak/IntroViewController.swift
@@ -23,6 +23,8 @@ class IntroViewController: UIViewController, UIImagePickerControllerDelegate, UI
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK: Camera Functions
+    
     @IBAction func beginButton(_ sender: Any) {
         let photoCapture = UIImagePickerController()
         
@@ -34,17 +36,38 @@ class IntroViewController: UIViewController, UIImagePickerControllerDelegate, UI
         present(photoCapture, animated: true,completion: nil)
         
 
-
+        
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    func beginButtonControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+//        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        
     }
-    */
+
 
 }
