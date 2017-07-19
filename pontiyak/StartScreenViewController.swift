c176ba1f1ef7aca07fa44565afc597f1ce9a7d6c
Stephen Mercer <u3158446@uni.canberra.edu.au>
2017-07-19 22:38:23 +1000

    Determines User already signed up

--- pontiyak/StartScreenViewController.swift
+++ pontiyak/StartScreenViewController.swift
@@ -9,39 +9,52 @@
 import UIKit
 
 class StartScreenViewController: UIViewController {
-
-    //TODO Fix error
-    var user = User()
-        
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        if getUser(){
+            
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "defaultView")
+            self.present(vc!, animated: true, completion: nil)
+            
+        } else{
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "welcomeView")
+            self.present(vc!, animated: true, completion: nil) //Causes Error
+            
+            // Do any additional setup after loading the view.
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     //MARK: Private Functions
     private func getUser()->Bool{
         
-        user = NSKeyedUnarchiver.unarchiveObject(withFile: user.ArchiveURL.path) as? User
         
-        return false
+        let user = NSKeyedUnarchiver.unarchiveObject(withFile: User.ArchiveURL.path) as? User
+        
+        if user != nil{
+            return true
+        }
+        else{
+            return false
+        }
     }
-
+    
 }
